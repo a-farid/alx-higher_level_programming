646362fc7cd8887dc7f5076c55ffa8bf0dfe6607
faridlord10 <fahizoune10@gmail.com>
2023-08-13 16:49:58 -0700

    7th task

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+
+    if len(tuple_a) == 0:
+        tupleA = (0, 0)
+    elif len(tuple_a) == 1:
+        tupleA = tuple_a[0] + 0
+    else:
+        tupleA = (tuple_a[0], tuple_a[1])
+
+    if len(tuple_b) == 0:
+        tupleB = (0, 0)
+    elif len(tuple_b) == 1:
+        tupleB = (tuple_a[0], 0)
+    else:
+        tupleB = (tuple_b[0], tuple_b[1])
+
+    valueA = tupleA[0] + tupleB[0]
+    valueB = tupleA[1] + tupleB[1]
+    return (valueA, valueB)
