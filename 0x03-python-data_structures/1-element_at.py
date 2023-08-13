ba363c6ba2a4e6bfbca623fc391a6896431cd89e
faridlord10 <fahizoune10@gmail.com>
2023-08-13 12:02:10 -0700

    add format2

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,18 +1,5 @@
 #!/usr/bin/python3
-
-"""
-    Get an element from a list
-
-    Args:
-        my_list: list
-        idx: Index of element
-
-    return:
-        Element from list
-"""
-
-
 def element_at(my_list, idx):
     if (idx < 0 or idx > len(my_list)):
         return None
-    return "{}".format(my_list[idx])
+    return "{:d}".format(my_list[idx])
