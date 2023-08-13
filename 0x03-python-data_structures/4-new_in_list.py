24f5929e6eb858470f41e32e05ff762fefa6ee79
faridlord10 <fahizoune10@gmail.com>
2023-08-13 15:39:05 -0700

    fix bug

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    newList = []
     if my_list:
-        my_list[idx] = element
-    return my_list
+        for i in range(len(my_list)):
+            if i == idx:
+                newList.append(element)
+            else:
+                newList.append(my_list[i])
+    return newList
