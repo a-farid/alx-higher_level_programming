9205faf14ef21f306fecaa272ad61867d44ee050
faridlord10 <fahizoune10@gmail.com>
2023-08-29 23:13:47 -0700

    Adv 1 2

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 
-'''Check if the value is Integer and print it if true'''
+import sys
+
+
 
 def safe_print_integer_err(value):
+    '''Check if the value is Integer and print it if true'''
+
     try:
         print("{:d}".format(value))
         return True
     except (ValueError, TypeError):
-        print("Exception: {}".format(sys.exc_info()[1]), file = sys.stderr)
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return False
-    
