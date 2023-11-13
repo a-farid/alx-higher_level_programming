3b794a3f95de8bdf4edd49874495ae14ebff6d46
faridlord10 <fahizoune10@gmail.com>
2023-11-13 13:15:52 -0800

    task 03

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-if (argv.length === 3) {
+const argvLen = process.argv.length;
+
+if (argvLen === 3) {
   console.log('Argument found');
-} else if (argv.length === 2) {
+} else if (argvLen === 2) {
   console.log('No argument');
 } else {
   console.log('Arguments found');
 }
-
-
