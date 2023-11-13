4282312c60f164e8e0628880a2e18b15947db1da
faridlord10 <fahizoune10@gmail.com>
2023-11-13 15:08:41 -0800

    rest of tasks 2

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,2 @@
 #!/usr/bin/node
-const argvLen = process.argv;
-console.log(`${argvLen[2]} is ${argvLen[3]}`);
-
+console.log(process.argv[2] + ' is ' + process.argv[3]);
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const amyVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (i of amyVar) console.log(i);
+for (let i of amyVar) console.log(i);
 
