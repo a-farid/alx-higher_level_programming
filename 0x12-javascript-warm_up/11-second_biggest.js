4d6eb4e1a75f39e094a4a4ac8c00d4be19679629
faridlord10 <fahizoune10@gmail.com>
2023-11-13 14:58:17 -0800

    rest of tasks 2

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
-const myVar = process.argv;
-let arrNumber = myVar
-  .slice(2)
-  .map((n) => Number(n))
-  .sort((a, b) => b - a);
-if (arrNumber[1] == undefined) {
-  console.log(0);
+if (process.argv.length <= 3) {
+  console.log('0');
 } else {
-  console.log(arrNumber[1]);
+  const arr = process.argv.slice(2).map(Number);
+  const second = arr.sort(function (a, b) { return b - a; })[1];
+  console.log(second);
 }
-
