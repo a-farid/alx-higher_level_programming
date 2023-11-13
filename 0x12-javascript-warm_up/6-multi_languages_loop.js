8bdfdaddb689746ce4066d9b19ef615eb0ed0530
faridlord10 <fahizoune10@gmail.com>
2023-11-13 15:13:08 -0800

    rest of tasks 2

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,3 +1,4 @@
 #!/usr/bin/node
-const amyVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i of amyVar) console.log(i);
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(line);
+}
