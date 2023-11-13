c74b311c822945a57d7301c0c85db2f287711299
faridlord10 <fahizoune10@gmail.com>
2023-11-13 14:42:41 -0800

    rest of tasks

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,6 +4,6 @@ const argvLen = process.argv;
 if (isNaN(argvLen[2])) {
   console.log('Not a number');
 } else {
-  console.log(`My number: ${argvLen[2]}`);
+  console.log(`My number: ${Number(argvLe([2])}`);
 }
 
