c2b49a57a1c4269d3edfe65b2850d793bcd2ac3e
faridlord10 <fahizoune10@gmail.com>
2023-11-14 06:19:40 -0800

    Advanced 02

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const dict = require('./101-data.js').dict;
+
+const keys = Object.keys(dict);
+const values = Object.values(dict);
+
+const resultObject = {};
+for (let i = 0; i < values.length; i++) {
+  if (!resultObject.hasOwnProperty(values[i])) {
+    resultObject[values[i]] = [keys[i]];
+  } else {
+    resultObject[values[i]] = [...resultObject[values[i]], keys[i]];
+  }
+}
+console.log(resultObject);
