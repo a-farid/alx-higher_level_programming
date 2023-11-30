6c1c30cbd092fc5eb1fa27332ab74faaca72a89e
faridlord10 <fahizoune10@gmail.com>
2023-11-30 13:49:40 -0800

    Task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Finds a peak in a list of unsorted integers"""
+
+
+def find_peak(list_of_integers):
+    """Finds a peak in list_of_integers"""
+
+    if list_of_integers == []:
+        return None
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    if len(list_of_integers) == 2:
+        return max(list_of_integers)
+    result = 0
+    for i in list_of_integers:
+        if i > result:
+            result = i
+    return result
