64c58c286fc1087886c5b901f86d517d927e56d0
faridlord10 <fahizoune10@gmail.com>
2023-12-01 13:50:57 -0800

    All T

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """A script that takes in a URL,
-- sends a request to a URL and displays the value
-  of the X-Request-Id variable found in the header ofthe response.
+  - sends a request to a URL and displays the value
+  of the X-Request-Id variable found in the header
+  ofthe response.
 """
 import sys
 import urllib.request
